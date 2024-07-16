f223e60ea9b779c9816749ea63f712ac0f4e71b0
philipp-burri <burri.philipp13@gmail.com>
2024-07-16 12:14:27 +0200

    navigation

--- src/utils/db.php
+++ src/utils/db.php
@@ -8,7 +8,7 @@ $db_pass = getenv("DB_PASSWORD");
 try {
 $dbConnection = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Connected successfully";
+
 } catch (PDOException $e) {
 echo $e->getMessage();
 
---
+++ src/utils/header.php
@@ -0,0 +1,4 @@
+<div class="header">
+<div class="name"><h2>Trivia Quiz</h2></div>
+<a href="index.php"><h2 class="home-btn">HOME</h2></a>
+</div>
