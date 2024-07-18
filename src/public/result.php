e9e80b878e5efb9dab1b3bb02887c9b749b20bb3
harryszh <harrys.zh12@gmail.com>
2024-07-18 09:22:19 +0200

    Resultat

--- src/public/result.php
+++ src/public/result.php
@@ -1,6 +1,36 @@
 <?php
-include '../utils/db.php';
 
-prettyPrint($_SESSION['points']);
+include 'assets/logik.php';
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Punkte Anzeige</title>
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="style.css">
+    <style>
+      
+        
+    </style>
+</head>
+<body>
+<?php include '../utils/header.php' ?>
+
+<div class="quiz-container">
+    <h1>Quiz Resultat</h1>
+    <h4>Dein Ergebnis</h4>
+    <p><?php echo isset($_SESSION['points']) ? $_SESSION['points'] : 0; ?> Punkte</p>
+</div>
+
+
+
+      
+
+<?php include '../utils/footer.php'; ?>
+</body>
+</html>
